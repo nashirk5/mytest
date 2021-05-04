efc31cc1f9d3e8f69abef22dace9559e9d32cf8b
Nashir <nashirk5@gmail.com>
2021-05-04 18:06:42 +0530

    Added input validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { ViewEncapsulation } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { validateBasis } from '@angular/flex-layout';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
@@ -21,10 +22,10 @@ export class AppComponent implements OnInit {
     this.myForm = this.fb.group({
       date: ['', Validators.required],
       time: ['', Validators.required],
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
+      firstName: ['', [Validators.required, Validators.pattern('^[a-zA-Z \-\']+')]],
+      lastName: ['', [Validators.required, Validators.pattern('^[a-zA-Z \-\']+')]],
       zipCode: ['', Validators.required],
-      phone: ['', Validators.required],
+      phone: ['', [Validators.required, Validators.pattern('[0-9]{3}-[0-9]{3}-[0-9]{4}')]],
       email: ['', Validators.required],
       drivingLocation: ['', Validators.required],
       address: ['', Validators.required],
@@ -35,9 +36,35 @@ export class AppComponent implements OnInit {
 
   }
 
+  onPhoneChages(){
+    var value = this.myForm.controls['phone'].value;
+    
+    if (value.length <= 10) {
+      var country, city, number, result;
+
+      country = value.slice(0, 3);
+      city = value.slice(3, 6);
+      number = value.slice(6, 10);
+
+      result = `(${country}) ${city} - ${number}`;
+
+      console.log(result);      
+
+      // if (result) {
+      //   this.myForm.controls['phone'].setValue(result);
+      // }
+    }   
+  }
+
   onSubmit() {
     console.log(this.myForm.value);
     this.result = this.myForm.value;
+
+    if(this.myForm.valid){
+      alert('Form has been submited')
+    } else {
+      alert('Please fill the form correctly')
+    }
   }
 
 }
